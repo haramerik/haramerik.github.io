8f1277461d762f1b3208b501c0bd29f9a5bb8613
Haramerik <amerik.bains@gmail.com>
2021-07-18 22:42:13 -0700

    solution 4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,6 @@
 var run = function () {
 
-var names = ["Yaakov", "John"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Jim", "Paul", "Frank", "Larry", "Paula", "Laura"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
